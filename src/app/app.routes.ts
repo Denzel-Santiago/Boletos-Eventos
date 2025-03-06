4ad9a7f06439ac46d84038dec5cec07cfa3c0373
Denzel-Santiago <233370@ids.upchiapas.edu.mx>
2025-03-06 15:09:30 -0600

    errores en el front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/Login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },
+    { path: 'Login', component: LoginComponent }
+
+
+];
---
+++ src/app/pages/Login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
